ac6a404ecc2f887219a28906085aa21a1671ad45
adam-clark-git <adam.b.clark@icloud.com>
2026-09-23 20:37:50 -0600

    Basics for ChessPiece done

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,10 +10,28 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
-
+    private ChessGame.TeamColor pieceColor;
+    private ChessPiece.PieceType type;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor =  pieceColor;
+        this.type = type;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+        ChessPiece other = (ChessPiece) obj;
+        return pieceColor == other.getTeamColor() && type == other.getPieceType();
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
     /**
      * The various different chess piece options
      */
@@ -29,14 +48,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
