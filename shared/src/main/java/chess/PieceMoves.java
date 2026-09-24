40760a2ce05b067e6313acd0ae8746c53427ef04
adam-clark-git <adam.b.clark@icloud.com>
2026-09-23 21:36:01 -0600

    PieceMoves added

---
+++ shared/src/main/java/chess/PieceMoves.java
@@ -0,0 +1,66 @@
+package chess;
+import java.util.*;
+import java.util.function.BiFunction;
+
+public class PieceMoves {
+    private ChessPosition pos;
+    private ChessGame.TeamColor color;
+    private ChessBoard board;
+    public PieceMoves(ChessPosition pos, ChessGame.TeamColor color, ChessBoard board) {
+        this.pos = pos;
+        this.color = color;
+        this.board = board;
+    }
+    public enum Direction {
+        NORTH,
+        NORTHEAST,
+        EAST,
+        SOUTHEAST,
+        SOUTH,
+        SOUTHWEST,
+        WEST,
+        NORTHWEST
+    }
+    public Collection<ChessMove> getRookMoves() {
+        BiFunction<Direction, Integer, ChessMove> movement = (dir, num) -> {
+            switch (dir) {
+                case NORTH -> {return new ChessMove(pos, new ChessPosition(pos.getRow()+num, pos.getColumn()), null);}
+                case SOUTH -> {return new ChessMove(pos, new ChessPosition(pos.getRow()-num, pos.getColumn()), null);}
+                case EAST -> {return new ChessMove(pos, new ChessPosition(pos.getRow(), pos.getColumn()+num), null);}
+                case WEST -> {return new ChessMove(pos, new ChessPosition(pos.getRow(), pos.getColumn()-num), null);}
+            }
+            return null;
+        };
+        return straightMoves(movement);
+    }
+    private Collection<ChessMove> straightMoves(BiFunction<Direction, Integer, ChessMove> movement) {
+        List<ChessMove> moveList = new ArrayList<ChessMove>();
+        for (Direction dir : Direction.values()) {
+            for (int i = 1; i < 9; i++) {
+                ChessMove move = movement.apply(dir, i);
+                if (move == null) {
+                    break;
+                }
+                if (isMoveValid(move)) {
+                    moveList.add(move);
+                    if (isMoveCapture(move)) {
+                        break;
+                    }
+                }
+                else break;
+            }
+
+        }
+        return moveList;
+    }
+    private boolean isMoveValid (ChessMove move) {
+        if (move.getEndPosition().getRow() > 8 || move.getEndPosition().getColumn() > 8 || move.getEndPosition().getRow() < 1 || move.getEndPosition().getColumn() < 1) {
+            return false;
+        }
+        return board.getPiece(move.getEndPosition()) == null || board.getPiece(move.getEndPosition()).getTeamColor() != color;
+    }
+    private boolean isMoveCapture (ChessMove move) {
+        return board.getPiece(move.getEndPosition()) != null && board.getPiece(move.getEndPosition()).getTeamColor() != color;
+    }
+
+}
